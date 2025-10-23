be63ec8a47b80ade41c02b572a4ca4c7a22f04e4
Paweł Fotta <79049103+OPYHL@users.noreply.github.com>
2025-10-23 23:14:53 +0200

    Add question catalog and operator timer controls

---
+++ src/main/java/pl/opyhl/oneoften/model/question/QuestionEntry.java
@@ -0,0 +1,31 @@
+package pl.opyhl.oneoften.model.question;
+
+public class QuestionEntry {
+    private String id;
+    private String question;
+    private String answer;
+
+    private String difficulty;
+    private String category;
+    private int order;
+
+    public QuestionEntry() {}
+
+    public String getId(){ return id; }
+    public void setId(String id){ this.id = id; }
+
+    public String getQuestion(){ return question; }
+    public void setQuestion(String question){ this.question = question; }
+
+    public String getAnswer(){ return answer; }
+    public void setAnswer(String answer){ this.answer = answer; }
+
+    public String getDifficulty(){ return difficulty; }
+    public void setDifficulty(String difficulty){ this.difficulty = difficulty; }
+
+    public String getCategory(){ return category; }
+    public void setCategory(String category){ this.category = category; }
+
+    public int getOrder(){ return order; }
+    public void setOrder(int order){ this.order = order; }
+}
